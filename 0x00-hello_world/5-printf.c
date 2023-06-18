cf334cb9a784aac511f7b3096204ab8dd31f313e
Edwin-Ogolla <rensonedwin@gmail.com>
2023-06-18 05:34:59 -0700

    Hello world task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main- This program prints the message in quotes
+ *
+ * Return: (0) if successful
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
