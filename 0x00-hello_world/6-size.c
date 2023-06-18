35fc71aba642b17d355d0b1a1e52926553d67968
Edwin-Ogolla <rensonedwin@gmail.com>
2023-06-18 06:10:35 -0700

    Hello world task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char is: %d bytes\n", sizeof(char));
 	printf("Size of int is: %d bytes\n", sizeof(int));
 	printf("Size of long int is: %d bytes\n", sizeof(long int));
-	printf("Size of long long is: %d bytes\n", sizeof(long long));
+	printf("Size of long long int is: %d bytes\n", sizeof(long long int));
 	printf("Size of float is: %d bytes\n", sizeof(float));
 	return (0);
 }
