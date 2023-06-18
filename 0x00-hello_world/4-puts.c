dfdaaf21053f3874d3bfb545dd65c057a0585dc3
Edwin-Ogolla <rensonedwin@gmail.com>
2023-06-18 04:52:19 -0700

    Hello, world task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+
 /**
  * main is the entry point
  *
  * return (0)  is always (0) if successful
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
