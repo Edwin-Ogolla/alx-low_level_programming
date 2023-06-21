46ef1673872ab8afed1351b229cfdeab55f92c38
Edwin-Ogolla <rensonedwin@gmail.com>
2023-06-21 10:48:02 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	for (alphabet = 'a'; alphabet <= 'f'; alphabet ++)
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
 		putchar (alphabet);
 	putchar('\n');
 	return (0);
