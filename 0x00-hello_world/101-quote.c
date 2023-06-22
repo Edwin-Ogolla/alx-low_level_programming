3074293c8f96577aa3a947892fb5b3e3117a0e4d
Edwin-Ogolla <rensonedwin@gmail.com>
2023-06-21 21:10:18 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints That piece of art is useful
- *
+ * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * 		followed by a new line
  * Return: Always 1
  */
 int main(void)
 {
 	write(2,
-			"and that piece of art is useful\n"-Dora Korpar, 2015-10-19\n",
-		        59);
+		"and that piece of art is useful" - Dora Korpar, 2015-10-19",
+		59);
 	return (1);
 }
